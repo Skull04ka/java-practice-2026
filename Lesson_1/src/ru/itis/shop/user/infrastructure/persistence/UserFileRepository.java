94b446f4aeac49c53f753e87b589c72d5b161a50
Skull04ka <tester232665@gmail.com>
2026-06-30 03:44:18 +0500

    nthng

---
+++ Lesson_1/src/ru/itis/shop/user/infrastructure/persistence/UserFileRepository.java
@@ -0,0 +1,50 @@
+package ru.itis.shop.user.infrastructure.persistence;
+
+import ru.itis.shop.user.domain.User;
+import ru.itis.shop.user.repository.UserRepository;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.UUID;
+
+public class UserFileRepository implements UserRepository {
+
+    private final String fileName;
+
+    public UserFileRepository(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public void save(User user) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            String id = UUID.randomUUID().toString();
+            user.setId(id);
+            writer.write(user.getId() + "|" +
+                    user.getEmail() + "|" +
+                    user.getPassword() + "|" +
+                    user.getProfileDescription());
+            writer.newLine();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public User findById(String id) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            while (reader.ready()) {
+                String line = reader.readLine();
+                String[] arr = line.split("\\|");
+                if (arr[0].equals(id)) {
+                    return new User(arr[0], arr[1], arr[2], arr[3]);
+                }
+            }
+            return null;
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
